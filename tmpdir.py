0c432795528548f6d13b8cb9095dfb8f4536fa1a
AdebayoBraimah <adebayo.braimah@gmail.com>
2022-05-06 21:34:04 -0400

    RF: Split this code into two different modules

--- tmpdir.py
+++ tmpdir.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-"""Temporary working directory and file module for the ``commandio`` package.
+"""Temporary working directory module for the ``commandio`` package.
 """
 import os
 import random
 
-from typing import Optional
-
 from workdir import WorkDir
-from fileio import File
 
 
 class TmpDir(WorkDir):
@@ -32,7 +29,7 @@ class TmpDir(WorkDir):
             "/path/to/temporary_directory"
             >>> tmp_dir.rmdir(rm_parent=False)
 
-    Arguments:
+    Args:
         src: Temporary parent directory name/path.
         use_cwd: Use current working directory as working direcory.
     """
@@ -52,7 +49,7 @@ class TmpDir(WorkDir):
             "/path/to/temporary_directory"
             >>> tmp_dir.rmdir(rm_parent=False)
 
-        Arguments:
+        Args:
             src: Temporary parent directory name/path.
             use_cwd: Use current working directory as working direcory.
         """
@@ -68,64 +65,3 @@ class TmpDir(WorkDir):
         """Context manager exit method for ``TmpDir`` class."""
         self.rmdir()
         return super().__exit__(exc_type, exc_val, traceback)
-
-
-class TmpFile(File):
-    """Creates and manipulates temporary files via inheritance from the ``File`` object base class.
-
-    Attributes:
-        file: Temporary file name.
-        ext: File extension of input file. If no extension is provided, it is inferred.
-
-    Usage example:
-        >>> tmp_directory = TmpDir("/path/to/temporary_directory")
-        >>>
-        >>> temp_file = TmpFile(tmp_directory.tmp_dir,
-        ...                             ext="txt")
-        ...
-        >>> temp_file
-        "/path/to/temporary_directory/temporary_file.txt"
-
-    Arguments:
-        tmp_dir: Temporary directory name.
-        tmp_file: Temporary file name.
-        ext: Temporary directory file extension.
-    """
-
-    def __init__(
-        self,
-        tmp_dir: str,
-        tmp_file: Optional[str] = "",
-        ext: Optional[str] = "",
-    ) -> None:
-        """Initialization method for the TmpFile class that inherits from the ``File`` base class, allowing for the creation and maninuplation of temporary files.
-
-        Usage example:
-            >>> tmp_directory = TmpDir("/path/to/temporary_directory")
-            >>>
-            >>> temp_file = TmpFile(tmp_directory.tmp_dir,
-            ...                             ext=".txt")
-            ...
-            >>> temp_file
-            "/path/to/temporary_directory/temporary_file.txt"
-
-        Arguments:
-            tmp_dir: Temporary directory name.
-            tmp_file: Temporary file name.
-            ext: File extension.
-        """
-        if tmp_file:
-            pass
-        else:
-            _n: int = 10000
-            tmp_file: str = "tmp_file_" + str(random.randint(0, _n))
-
-        if ext:
-            if "." in ext:
-                pass
-            else:
-                ext: str = f".{ext}"
-            tmp_file: str = tmp_file + f"{ext}"
-
-        tmp_file: str = os.path.join(tmp_dir, tmp_file)
-        super(TmpDir.TmpFile, self).__init__(tmp_file, ext)
